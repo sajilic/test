a410337047c9ff1cf4ae6fbbd6ae5dc75398ddd2
sajilic <sajilichakraborty2k02@gmail.com>
2024-02-03 18:01:49 +0530

    feat: test

---
+++ conv.js
@@ -0,0 +1,20 @@
+let name ="sajili"
+let age=21
+let s=null
+let d=undefined
+// console.log(typeof "sajili");
+// console.log(typeof age);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+//Conversion:
+
+let score="33"
+console.log(typeof score)
+let valueInScore= Number(score)
+console.log(typeof valueInScore)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
---
+++ index.js
@@ -0,0 +1,10 @@
+const accountId=14452
+let accountEmail="sajili.chak@gmail.com"
+var accountPassword="12345" // do not use var because of the isuue of functional scope and block scope 
+accountCity="Kolkata" //without using any variables there is a possibility but that certainly is not safe 
+
+accountEmail="sajii2k02@gmail.com"
+accountPassword="12"
+accountCity="Bangalore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
